833b6788f8b20edc9f60c1ff47cd54fca3d71423
MartinWachira <67173409+mmwachira@users.noreply.github.com>
2024-03-30 15:41:10 +0200

    feat: Writing markup with JSX

--- react-fundamentals/src/App.tsx
+++ react-fundamentals/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Button from './components/Button';
+import AboutPage from './components/AboutPage';
 
 function App() {
   return (
     <div>
       <h1>Hello, I just made my first button. Here it is.</h1>
       <Button />
+      <AboutPage />
     </div>
   );
 }
---
+++ react-fundamentals/src/components/AboutPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function AboutPage(){
+
+    return(
+        <>
+    <h1>About</h1>
+    <p>Hello, my name is Martin Wachira.<br/>How do you do?</p>
+    
+    </>
+    )
+    
+}
+
+export default AboutPage;
