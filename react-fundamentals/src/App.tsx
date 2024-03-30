8d8fe2c31c9eac33c3d5a6418473d9af6dba08b9
MartinWachira <67173409+mmwachira@users.noreply.github.com>
2024-03-30 15:35:18 +0200

    feat: Creating and Nesting Components

---
+++ react-fundamentals/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, I just made my first button. Here it is.</h1>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
