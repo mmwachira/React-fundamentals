94cdf69372e710cc77f284575d4c714f744417d7
MartinWachira <67173409+mmwachira@users.noreply.github.com>
2024-03-31 09:49:47 +0300

    feat: Conditional rendering

--- react-fundamentals/src/App.tsx
+++ react-fundamentals/src/App.tsx
@@ -2,6 +2,14 @@ import './App.css';
 import Button from './components/Button';
 import AboutPage from './components/AboutPage';
 
+let isLoggedIn;
+
+let content: any;
+if (isLoggedIn) {
+  content = <AboutPage />
+} else {
+  content = <Button />
+}
 
 function App() {
   return (
@@ -9,6 +17,9 @@ function App() {
       <h1 className='header'>Hello, I just made my first button. Here it is.</h1>
       <Button />
       <AboutPage />
+      <br />
+
+      {content}
     </div>
   );
 }
